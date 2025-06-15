d2d06fd2378470171699e350c03a271fe734a8af
Gladden Rumao <gladdenbackup@gmail.com>
2025-06-15 19:46:04 +0530

    Added bubble sort algorithm

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr){
+    let n = arr.length;
+
+    for(let iter=1 ; iter<=n-1; iter++){
+        for(let i=0 ; i<n-iter ; i++){
+        if(arr[i] > arr[i+1]){
+            let temp = arr[i]; 
+            arr[i] = arr[i+1]; 
+            arr[i+1] = temp;
+        }
+    }
+    }
+    
+}
+
+let arr = [4,3,5,2,1];
+bubbleSort(arr);
+console.log('Sorted array is :', arr);
